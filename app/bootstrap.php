52e543c6436d1cabcd5d3b7139267748ae31e0de
Blaz Petek <blazpetek@BlazMac.local>
2017-05-31 13:18:53 +0200

    Instantiating ,  and  objects in bootstrap file, rather than in each view seperately

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -7,4 +7,9 @@ include APP_PATH . '/models/Db.php';
 include APP_PATH . '/models/Article.php';
 include APP_PATH . '/models/User.php';
 
+
+$db = new Db;
+$article = new Article($db);
+$user = new User($db);
+
 ?>
