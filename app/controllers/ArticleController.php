85366151c0a380ab78782c85992b98a8b8fef346
Blaz Petek <petekblaz94@gmail.com>
2017-05-31 15:44:08 +0200

    Made controller directory

---
+++ app/controllers/ArticleController.php
@@ -0,0 +1,80 @@
+<?php
+
+class ArticleController
+{
+    private $articleModel;
+    /**
+     * Set property $db to argument, which
+     * has to be an instance of Db object
+     *
+     * @param Db $db Object Db
+     */
+    public function __construct(Article $articleModel)
+    {
+        $this->articleModel = $articleModel;
+    }
+
+    /**
+     * Get custom number of articles from database, newest first
+     *
+     * @param  int $numberOfArticles number of articles
+     *
+     * @return array                 Array of all articles
+     */
+    public function getArticles($numberOfArticles)
+    {
+        return $this->articleModel->getArticles($numberOfArticles);
+    }
+
+    /**
+     * Grabs article by its ID, validates it,
+     * and then returns that article
+     *
+     * @param  int $id ID of article
+     *
+     * @return array Associative array of articles
+     */
+    public function getArticleById($id)
+    {
+        if (is_int( (int) $id)) {
+            return $this->articleModel->getSingleArticleById($id);
+        }
+        else {
+            return false;
+        }
+
+    }
+
+    /**
+     * Checks if submitted new article form data is valid.
+     * For now, it only checks if everything was filled in
+     *
+     * @param  string $title    Article title
+     * @param  string $body     Article body
+     * @param  int    $authorId ID of author
+     *
+     * @return bool               Return true, if all fields were filled, false otherwise
+     */
+    public function validateArticleFormData($title, $body, $authorId)
+    {
+        if (!empty($title) && !empty($body) && is_int($authorId)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Save article to database
+     *
+     * @param  string $title    Article title
+     * @param  string $body     Article body
+     * @param  int    $authorId ID of author
+     *
+     * @return bool               Return true, if all fields were filled, false otherwise
+     */
+    public function saveArticle($title, $body, $authorId)
+    {
+
+    }
+}
