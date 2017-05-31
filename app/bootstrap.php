e8134d6edfc84840b22e66cb242e576bc73dddbe
Blaz Petek <petekblaz94@gmail.com>
2017-05-31 13:35:28 +0200

    Changing some comments

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -7,7 +7,7 @@ include APP_PATH . '/models/Db.php';
 include APP_PATH . '/models/Article.php';
 include APP_PATH . '/models/User.php';
 
-
+// Instantiating db model and controllers
 $db = new Db;
 $article = new Article($db);
 $user = new User($db);
