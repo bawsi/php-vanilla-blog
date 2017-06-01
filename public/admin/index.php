1de1d9e77600b998415ba3f95e81280acf441ee8
Blaz Petek <petekblaz94@gmail.com>
2017-06-01 17:18:55 +0200

    Fixed some tags and stuff, + table is now being filled with article id, title, category, author, created on and edit + delete buttons

--- public/admin/index.php
+++ public/admin/index.php
@@ -1,24 +1,41 @@
 <?php
 include(realpath($_SERVER['DOCUMENT_ROOT'] . '/../app/bootstrap.php'));
 
-
+$articles = $article->getArticles(999);
 
 include(TEMPLATES_PATH . '/_header.php')
 ?>
 
 <div class="container container-admin-index">
-    <table>
+    <table class="table table-condensed">
         <tr>
-            <td>id</td>
-            <td>Title</td>
-            <td>Category</td>
-            <td>Author</td>
-            <td>Created on</td>
+            <th>id</th>
+            <th>Title</th>
+            <th>Category</th>
+            <th>Author</th>
+            <th>Created on</th>
+            <th></th>
         </tr>
+
+        <?php foreach ($articles as $article):?>
+            <tr>
+                <td><?php echo htmlspecialchars($article['id']); ?></td>
+                <td><?php echo htmlspecialchars($article['title']); ?></td>
+                <td><?php echo htmlspecialchars($article['category_name']); ?></td>
+                <td><?php echo htmlspecialchars($article['author']); ?></td>
+                <td><?php echo htmlspecialchars(date("d.m.Y", $article['created_at'])); ?></td>
+                <td>
+                    <a href="#" class="btn btn-primary btn-xs">Edit</a>
+                    <a href="#" class="btn btn-danger btn-xs">Delete</a>
+                </td>
+            </tr>
+
+        <?php endforeach; ?>
+
     </table>
 
 
 </div>
 
 
-<?php include(TEMPLATES_PATH . '/_footer.php') ?>
+<?php include(TEMPLATES_PATH . '/_footer.php'); ?>
