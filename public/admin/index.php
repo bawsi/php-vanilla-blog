cf8268f164af8b48b5455557dcec3030f5571788
Blaz Petek <petekblaz94@gmail.com>
2017-06-01 16:51:13 +0200

    Started work on admin index page. Added basic template + started work on table for articles

--- public/admin/index.php
+++ public/admin/index.php
@@ -1,1 +1,24 @@
-<?php echo "hi"; ?>
+<?php
+include(realpath($_SERVER['DOCUMENT_ROOT'] . '/../app/bootstrap.php'));
+
+
+
+include(TEMPLATES_PATH . '/_header.php')
+?>
+
+<div class="container container-admin-index">
+    <table>
+        <tr>
+            <td>id</td>
+            <td>Title</td>
+            <td>Category</td>
+            <td>Author</td>
+            <td>Created on</td>
+        </tr>
+    </table>
+
+
+</div>
+
+
+<?php include(TEMPLATES_PATH . '/_footer.php') ?>
