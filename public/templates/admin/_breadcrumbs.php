3d1ad7b1d4f9e70a1ce6ae40da82efd54190013d
Blaz Petek <petekblaz94@gmail.com>
2017-06-02 09:59:21 +0200

    added basic breadcrumbs bootstrap code

---
+++ public/templates/admin/_breadcrumbs.php
@@ -0,0 +1,9 @@
+<div class="row">
+    <div class="col-md-6 col-md-offset-3">
+        <!-- breadcrumbs -->
+        <ol class="breadcrumb">
+            <li><a href="#">Admin panel</a></li>
+            <li class="active"><a href="#">Article list</a>
+        </ol>
+    </div>
+</div>
