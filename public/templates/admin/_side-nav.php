b8dc11f8a0fddb4d56effdb07591d38870d5b228
Blaz Petek <petekblaz94@gmail.com>
2017-06-02 10:20:00 +0200

    Now echoing 'active' on correct menu item + menu items now link to correct pages

--- public/templates/admin/_side-nav.php
+++ public/templates/admin/_side-nav.php
@@ -1,8 +1,8 @@
 <div class="col-md-3">
     <div class="list-group">
-        <a href="#" class="list-group-item">Home</a>
-        <a href="#" class="list-group-item">List articles</a>
-        <a href="#" class="list-group-item">New article</a>
-        <a href="#" class="list-group-item">Users</a>
+        <a href="/admin" class="list-group-item<?php echo ($page == 'admin-home' ? ' active' : ''); ?>">Home</a>
+        <a href="/admin/article-index.php" class="list-group-item<?php echo ($page == 'admin-article-index' ? ' active' : ''); ?>">Article index</a>
+        <a href="/admin/new-article.php" class="list-group-item<?php echo ($page == 'admin-new-article' ? ' active' : ''); ?>">New article</a>
+        <a href="/admin/users.php" class="list-group-item<?php echo ($page == 'admin-users' ? ' active' : ''); ?>">Users</a>
     </div>
 </div>
