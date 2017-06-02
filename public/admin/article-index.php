1fc80802c8bf47eec40d57cc8cbb8f6b39e2dc29
Blaz Petek <petekblaz94@gmail.com>
2017-06-02 17:15:28 +0200

    Connected delete article buttons

--- public/admin/article-index.php
+++ public/admin/article-index.php
@@ -33,7 +33,7 @@ include(TEMPLATES_PATH . '/_header.php')
                         <td><?php echo htmlspecialchars(date("d.m.Y", $article['created_at'])); ?></td>
                         <td>
                             <a href="#" class="btn btn-primary btn-xs"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></a>
-                            <a href="#" class="btn btn-danger btn-xs"><i class="fa fa-times" aria-hidden="true"></i></a>
+                            <a href="<?php echo '/admin/delete.php?id=' . $article['id']; ?>" class="btn btn-danger btn-xs"><i class="fa fa-times" aria-hidden="true"></i></a>
                         </td>
                     </tr>
                 <?php endforeach; ?>
