afce4d5a498aa6cb2cf4b76dd07eff791c96ce29
Blaz Petek <petekblaz94@gmail.com>
2017-06-02 09:58:17 +0200

    Added basic side navigation

---
+++ public/templates/admin/_side-nav.php
@@ -0,0 +1,8 @@
+<div class="col-md-3">
+    <div class="list-group">
+        <a href="#" class="list-group-item">Home</a>
+        <a href="#" class="list-group-item">List articles</a>
+        <a href="#" class="list-group-item">New article</a>
+        <a href="#" class="list-group-item">Users</a>
+    </div>
+</div>
