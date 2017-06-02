4c9253899f8411d2b89c85c44afc3691fe89df11
Blaz Petek <petekblaz94@gmail.com>
2017-06-02 09:19:57 +0200

    Added link to admin panel

--- public/templates/_navbar.php
+++ public/templates/_navbar.php
@@ -13,6 +13,7 @@
 			<ul class="nav navbar-nav">
 				<li class="active"><a href="/">Home</a></li>
 				<li><a href="#contact">Contact</a></li>
+				<li><a href="/admin">Admin panel</a></li>
 			</ul>
 		</div><!--/.nav-collapse -->
 	</div>
