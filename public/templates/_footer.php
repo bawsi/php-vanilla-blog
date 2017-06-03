beee9d7c57c48fe481aa6909da5d43302df7c1f5
Blaz Petek <petekblaz94@gmail.com>
2017-06-03 10:57:57 +0200

    Modified footer with a bit more style

--- public/templates/_footer.php
+++ public/templates/_footer.php
@@ -1,3 +1,9 @@
+<footer class="footer">
+    <div class="container">
+        <p class="text-muted text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit, ex.</p>
+    </div>
+</footer>
+
 <!-- Bootstrap js -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </body>
