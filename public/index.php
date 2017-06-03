3b857c559da49938d69a2df26163300d47028e5b
Blaz Petek <petekblaz94@gmail.com>
2017-06-03 10:36:20 +0200

    Pagination added

--- public/index.php
+++ public/index.php
@@ -1,7 +1,12 @@
 <?php
 include(realpath($_SERVER['DOCUMENT_ROOT'] . '/../app/bootstrap.php'));
 
-$articles = $article->getArticles(9);
+// pagination
+$page = (isset($_GET['p'])) ? $_GET['p'] : 1;
+$perPage = 9;
+$totalPages = $article->getTotalNumberOfPages($perPage);
+
+$articles = $article->getArticlesPaginated($page, $perPage);
 
 include(TEMPLATES_PATH . '/_header.php')
 ?>
@@ -12,7 +17,6 @@ include(TEMPLATES_PATH . '/_header.php')
 	<hr width="75%">
 	<!-- Articles row -->
 	<div class="row">
-
 		<?php foreach ($articles as $article): ?>
 			<!-- Single article -->
 			<div class="single-article col-md-4 col-sm-6 col-xs-12">
@@ -46,6 +50,39 @@ include(TEMPLATES_PATH . '/_header.php')
 		<?php endforeach; ?>
 
 	</div> <!-- End of articles row -->
+
+	<!-- pagination -->
+	<nav aria-label="Page navigation" class="text-center">
+		<ul class="pagination">
+
+			<?php if($page > 1): ?>
+				<li>
+					<a href="/index.php?p=<?php echo $page - 1; ?>" aria-label="Previous">
+						<span aria-hidden="true">&laquo;</span>
+					</a>
+				</li>
+			<?php endif; ?>
+
+			<?php for($pageCount = 1; $pageCount <= $totalPages; $pageCount++):?>
+				<li><a href="/index.php?p=<?php echo $pageCount; ?>"><?php echo $pageCount; ?></a></li>
+			<?php endfor; ?>
+
+			<?php if($page != $totalPages): ?>
+				<li>
+					<a href="/index.php?p=<?php echo $page + 1; ?>" aria-label="Next">
+						<span aria-hidden="true">&raquo;</span>
+					</a>
+				</li>
+			<?php endif; ?>
+
+		</ul>
+	</nav>
+
+
+
+
+
+
 </div> <!-- End of main content -->
 
 <?php include(TEMPLATES_PATH . '/_footer.php') ?>
