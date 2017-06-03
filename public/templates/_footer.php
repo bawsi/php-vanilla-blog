59a8a01c327a5a19e6f6679efc866038f107e9f5
Blaz Petek <petekblaz94@gmail.com>
2017-06-03 16:22:21 +0200

    Added copyright message

--- public/templates/_footer.php
+++ public/templates/_footer.php
@@ -1,6 +1,6 @@
-<footer class="footer">
+<footer class="footer" style="background: #2c3e50; color: white; padding-top: 10px;">
     <div class="container">
-        <p class="text-muted text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit, ex.</p>
+        <p class="text-muted text-center">Copyright © 2017 Blaz Petek</p>
     </div>
 </footer>
 
