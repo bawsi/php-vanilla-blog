57b587abf6d5a49fa6b7d8cade836305d123b0ff
Blaz Petek <petekblaz94@gmail.com>
2017-06-04 19:03:55 +0200

    Added image upload to form, to update thumbnail img

--- public/admin/edit.php
+++ public/admin/edit.php
@@ -7,9 +7,10 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $articleId = $_POST['articleId'];
     $title = $_POST['title'];
     $body = $_POST['body'];
+    $image = $_FILES['image'];
     $categoryId = $_POST['categoryId'];
 
-    if ($article->edit($articleId, $title, $body, $categoryId)) {
+    if ($article->edit($articleId, $title, $body, $image, $categoryId)) {
         $_SESSION['success_messages'][] = 'Article successfully updated!';
         header('location: /article.php?id=' . $articleId);
     } else {
@@ -38,12 +39,14 @@ include(TEMPLATES_PATH . '/_header.php');
 
 		<!-- edit article form column -->
 		<div class="col-md-9">
-			<form class="article-form" action="" method="post">
+			<form class="article-form" action="" method="post" enctype="multipart/form-data">
                 <input type="hidden" name="articleId" value="<?php echo $articleId; ?>">
 				<p>Article Title</p>
 				<input type="text" name="title" placeholder="Article title here" class="form-control" value="<?php echo $articleData['title']; ?>">
 				<p>Article Body</p>
 				<textarea name="body" rows="8"><?php echo $articleData['body']; ?></textarea>
+                <p>Image (appears on article thumbnail / 400x200)</p>
+				<input type="file" name="image" class="form-control" accept="image/*">
 				<p>Category</p>
 				<select class="category form-control" name="categoryId">
 					<?php foreach ($categories as $category): ?>
