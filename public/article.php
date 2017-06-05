ea87f9a8a8f4053416335f45658600bdfc8288b3
Blaz Petek <petekblaz94@gmail.com>
2017-06-05 19:33:12 +0200

    Variable name changes, since there was a conflict ($article to $articleData)

--- public/article.php
+++ public/article.php
@@ -11,13 +11,13 @@ include(TEMPLATES_PATH . '/_header.php');
 	<div class="container container-article">
 		<div class="row">
 			<div class="article-col col-md-10 col-md-offset-1">
-				<a href="<?php echo '/admin/delete.php?id=' . $article['id']; ?>" class="btn btn-danger btn-xs pull-right">Delete</a>
-				<a href="<?php echo '/admin/edit.php?id=' . $article['id']; ?>" class="btn btn-primary btn-xs pull-right">Edit</a>
-				<h2 class="article-title"><?php echo $article['title']; ?></h2>
+				<a href="<?php echo '/admin/delete.php?id=' . $articleData['id']; ?>" class="btn btn-danger btn-xs pull-right">Delete</a>
+				<a href="<?php echo '/admin/edit.php?id=' . $articleData['id']; ?>" class="btn btn-primary btn-xs pull-right">Edit</a>
+				<h2 class="article-title"><?php echo $articleData['title']; ?></h2>
 				<h5 class="article-info">
-					<span><i class="fa fa-calendar"></i> <?php echo htmlspecialchars(date('d.m.Y \a\t H:i', $article['created_at'])); ?></span>
-					<span><i class="fa fa-user"></i> <?php echo htmlspecialchars($article['author']); ?></span>
-					<span><i class="fa fa-folder-open-o"></i> <?php echo htmlspecialchars($article['category_name']); ?></span>
+					<span><i class="fa fa-calendar"></i> <?php echo htmlspecialchars(date('d.m.Y \a\t H:i', $articleData['created_at'])); ?></span>
+					<span><i class="fa fa-user"></i> <?php echo htmlspecialchars($articleData['author']); ?></span>
+					<span><i class="fa fa-folder-open-o"></i> <?php echo htmlspecialchars($articleData['category_name']); ?></span>
 				</h5>
 				<hr>
 				<p class="article-body"><?php echo $articleData['body']; ?></p>
