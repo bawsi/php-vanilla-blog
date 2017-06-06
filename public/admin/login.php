99db97a5569302f9a6f7f75f66398aeb80b5fb88
Blaz Petek <petekblaz94@gmail.com>
2017-06-06 09:30:26 +0200

    Removed bootstraps header,

--- public/admin/login.php
+++ public/admin/login.php
@@ -7,21 +7,14 @@ include(TEMPLATES_PATH . '/_header.php')
 <div class="container container-admin-login">
 
     <div class="row">
-        <div class="col-md-4 col-md-offset-3">
-            <div class="panel panel-success">
-                <div class="panel-heading">
-                    <h3 class="panel-title">Login to admin area</h3>
-                </div>
-                <div class="panel-body">
-                    <!-- login form -->
-                    <form action="" method="post">
-                        <!-- <p>Username:</p> -->
-                        <input type="text" class="form-control" name="username" placeholder="Username">
-                        <input type="password" class="form-control" name="password" placeholder="Password">
-                        <button type="submit" class="btn btn-primary btn-block" name="submit">Login</button>
-                    </form>
-                </div>
-            </div>
+        <div class="col-md-4 col-md-offset-4">
+            <form action="" method="post">
+                <!-- <p>Username:</p> -->
+                <input type="text" class="form-control" name="username" placeholder="Username">
+                <input type="password" class="form-control" name="password" placeholder="Password">
+                <button type="submit" class="btn btn-success btn-block" name="submit">Login</button>
+            </form>
+
         </div>
     </div>
 
