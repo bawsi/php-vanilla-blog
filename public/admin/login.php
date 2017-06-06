c5ffb7767d47dde63dbb88cfdb64a98c66961df9
Blaz Petek <petekblaz94@gmail.com>
2017-06-06 18:34:13 +0200

    Showing admin area username and password, so anyone can login, and check it out

--- public/admin/login.php
+++ public/admin/login.php
@@ -25,7 +25,8 @@ include(TEMPLATES_PATH . '/_header.php')
 
     <div class="row">
         <div class="col-md-4 col-md-offset-4">
-            <h1>Login to admin area</h1>
+            <h1 class="text-center">Login to admin area</h1>
+            <p class="text-center"><strong>Username:</strong> admin - <strong>Password:</strong> 123</p>
             <hr />
             <form class="login-form" action="" method="post">
                 <!-- <p>Username:</p> -->
