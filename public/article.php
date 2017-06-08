fe00a689ed5f32968fdacdca9866b5bc6efd0a7d
Blaz Petek <petekblaz94@gmail.com>
2017-06-08 23:03:03 +0200

    Added disquss commenting system to each article page

--- public/article.php
+++ public/article.php
@@ -27,6 +27,31 @@ include(TEMPLATES_PATH . '/_header.php');
 				<p class="article-body"><?php echo $articleData['body']; ?></p>
 			</div>
 		</div>
+
+		<!-- disqus comments -->
+		<div id="disqus_thread" class="comments">
+			<script>
+
+				/**
+				*  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+				*  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/
+				/*
+				var disqus_config = function () {
+				this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
+				this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+				};
+				*/
+				(function() { // DON'T EDIT BELOW THIS LINE
+					var d = document, s = d.createElement('script');
+					s.src = 'https://mysite-gbjknd3abq.disqus.com/embed.js';
+					s.setAttribute('data-timestamp', +new Date());
+					(d.head || d.body).appendChild(s);
+				})();
+			</script>
+			<noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+		</div>
+
+
 	</div> <!-- End of main content -->
 
 <?php else: ?>
