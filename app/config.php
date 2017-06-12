7b5a476d53a4052f86f60569aa52e4faf2212127
Blaz Petek <petekblaz94@gmail.com>
2017-06-12 13:50:41 +0200

    Added constant JWT_KEY, for jwt authentication

--- app/config.php
+++ app/config.php
@@ -10,6 +10,9 @@ define("PUBLIC_PATH", dirname(__DIR__) . '/public');
 define("APP_PATH", dirname(__DIR__) . '/app');
 define("TEMPLATES_PATH", dirname(__DIR__) . '/public/templates');
 
+// Other
+define('JWT_KEY', 'qwpdjqwd0912uepj12je-912f12fpojqsfl');
+
 // errors
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
