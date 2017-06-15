21e568279259a6c57bb490be4b60c46902bf0a8c
Blaz Petek <petekblaz94@gmail.com>
2017-06-15 13:53:37 +0200

    Now check if $articles is set, and not false, before showing articles, otherwise, show simple msg that nothing was found

--- public/search.php
+++ public/search.php
@@ -13,38 +13,43 @@ include(TEMPLATES_PATH . '/_header.php')
 	<hr width="75%">
 	<!-- Articles row -->
 	<div class="row">
-		<?php foreach ($articles as $article): ?>
-			<!-- Single article -->
-			<div class="single-article col-md-4 col-sm-6 col-xs-12">
-				<div class="thumbnail">
-					<a href="/article.php?id=<?php echo $article['id']; ?>"><img src="<?php echo $article['img_path']; ?>" alt="article image thumbnail"></a>
-					<div class="caption" style="padding-bottom: 0">
+		<?php if (isset($articles) && !empty($articles)): ?>
+			<?php foreach ($articles as $article): ?>
+				<!-- Single article -->
+				<div class="single-article col-md-4 col-sm-6 col-xs-12">
+					<div class="thumbnail">
+						<a href="/article.php?id=<?php echo $article['id']; ?>"><img src="<?php echo $article['img_path']; ?>" alt="article image thumbnail"></a>
+						<div class="caption" style="padding-bottom: 0">
 
-						<p class="article-info">
-							<span>
-								<i class="fa fa-calendar"></i>
-								<?php echo htmlspecialchars(date('d.m.Y', $article['created_at'])); ?>
-							</span>
+							<p class="article-info">
+								<span>
+									<i class="fa fa-calendar"></i>
+									<?php echo htmlspecialchars(date('d.m.Y', $article['created_at'])); ?>
+								</span>
 
-							<span>
-								<i class="fa fa-user"></i>
-								<?php echo htmlspecialchars($article['author']); ?>
-							</span>
+								<span>
+									<i class="fa fa-user"></i>
+									<?php echo htmlspecialchars($article['author']); ?>
+								</span>
 
-							<span class="category">
-								<i class="fa fa-folder-open-o"></i>
-								<a href="/category.php?c=<?php echo htmlspecialchars($article['category_name']); ?>"><?php echo htmlspecialchars($article['category_name']); ?></a>
-							</span>
-						</p>
+								<span class="category">
+									<i class="fa fa-folder-open-o"></i>
+									<a href="/category.php?c=<?php echo htmlspecialchars($article['category_name']); ?>"><?php echo htmlspecialchars($article['category_name']); ?></a>
+								</span>
+							</p>
 
-						<a class="article-title" href="/article.php?id=<?php echo $article['id']; ?>"><h3><?php echo $article['title']; ?></h3></a>
+							<a class="article-title" href="/article.php?id=<?php echo $article['id']; ?>"><h3><?php echo $article['title']; ?></h3></a>
+						</div>
+
+						<a href="article.php?id=<?php echo $article['id']; ?>" class="btn btn-block btn-primary read-more-btn" role="button">Read More</a>
+
 					</div>
+				</div> <!-- End of article -->
+			<?php endforeach; ?>
 
-					<a href="article.php?id=<?php echo $article['id']; ?>" class="btn btn-block btn-primary read-more-btn" role="button">Read More</a>
-
-				</div>
-			</div> <!-- End of article -->
-		<?php endforeach; ?>
+		<?php else: ?>
+			<h4 class="text-center">No articles matching your search query were found</h4>
+		<?php endif; ?>
 
 	</div> <!-- End of articles row -->
 
