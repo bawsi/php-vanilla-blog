8b47b6204791532cde80809c10b64ac968654580
Blaz Petek <petekblaz94@gmail.com>
2017-06-19 13:51:15 +0200

    Bootstrap included, and calling deleteUser method

---
+++ public/admin/delete-user.php
@@ -0,0 +1,10 @@
+<?php
+// bootstrap and page variables
+include(realpath($_SERVER['DOCUMENT_ROOT'] . '/../app/bootstrap.php'));
+$currentPage = 'admin';
+
+// If not logged in, redirect to login page
+$user->redirectIfNotLoggedIn();
+
+// Attempt to register new user
+$user->deleteUser();
