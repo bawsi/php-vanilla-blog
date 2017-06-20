eb4b9d0f50428c49a6f365a4b1d9882e1c28afc6
Blaz Petek <petekblaz94@gmail.com>
2017-06-20 12:09:12 +0200

    Only show Users tab in side-nav, if logged in user is admin

--- public/templates/admin/_side-nav.php
+++ public/templates/admin/_side-nav.php
@@ -3,6 +3,8 @@
         <a href="/admin" class="list-group-item<?php echo ($page == 'admin-home' ? ' active' : ''); ?>">Home</a>
         <a href="/admin/article-index.php" class="list-group-item<?php echo ($page == 'admin-article-index' ? ' active' : ''); ?>">Article index</a>
         <a href="/admin/new-article.php" class="list-group-item<?php echo ($page == 'admin-new-article' ? ' active' : ''); ?>">New article</a>
-        <a href="/admin/users.php" class="list-group-item<?php echo ($page == 'admin-users' ? ' active' : ''); ?>">Users</a>
+        <?php if ($user->getUserRole() === 'admin'): ?>
+            <a href="/admin/users.php" class="list-group-item<?php echo ($page == 'admin-users' ? ' active' : ''); ?>">Users</a>
+        <?php endif; ?>
     </div>
 </div>
