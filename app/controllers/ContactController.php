b8e2c9eaccd76212ecd5a320bbdb999e2bf7701f
Blaz Petek <petekblaz94@gmail.com>
2017-06-22 16:21:20 +0200

    Added google recaptcha verification

--- app/controllers/ContactController.php
+++ app/controllers/ContactController.php
@@ -15,7 +15,19 @@ class ContactController
     {
 
         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-            if (!empty($_POST['name']) && !empty($_POST['email']) && !empty($_POST['subject']) && !empty($_POST['body'])) {
+            if (!empty($_POST['name']) && !empty($_POST['email']) && !empty($_POST['subject']) && !empty($_POST['body'] && !empty($_POST['g-recaptcha-response']))) {
+                $captcha  = $_POST['g-recaptcha-response'];
+                $secretRecaptchaKey = SECRET_RECAPTCHA_KEY;
+                $userIp = $_SERVER['REMOTE_ADDR'];
+
+                $response = json_decode(file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret="
+                            . $secretRecaptchaKey . "&response=" . $captcha . "&remoteip=" . $userIp), true); // True means we get back array from json_decode, instead of object (false is default)
+
+                if (!$response['success']) {
+                    $this->msg->error('You failed the captcha.. Please, do not try to spam!', '/contact.php');
+                    die();
+                }
+
                 $fromName = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_STRING);
                 $fromMail = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
                 $subject  = filter_input(INPUT_POST, 'subject', FILTER_SANITIZE_STRING);
@@ -42,11 +54,11 @@ class ContactController
                     $this->msg->error('Message could not be sent:<ul><li>' . $this->mail->ErrorInfo . '</li></ul>', '/contact.php');
                     die();
                 } else {
-                    $this->msg->success('Message has been sent. I will do my best ro respond within 24 hours.', '/contact.php');
+                    $this->msg->success('Message has been sent. I will do my best to respond within 24 hours.', '/contact.php');
                     die();
                 }
             } else {
-                $this->msg->error('All fields are required.', '/contact.php');
+                $this->msg->error('All fields are required, including the captcha.', '/contact.php');
                 die();
             }
         }
