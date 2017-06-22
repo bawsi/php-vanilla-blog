21c2c7c8eef8c5e52f0b298d828b10ce1e1bcf02
Blaz Petek <petekblaz94@gmail.com>
2017-06-22 10:01:25 +0200

    Basic contact me page layout done

---
+++ public/contact.php
@@ -0,0 +1,34 @@
+<?php
+//bootstrap
+include(realpath($_SERVER['DOCUMENT_ROOT'] . '/../app/bootstrap.php'));
+$currentPage = 'contact';
+
+include(TEMPLATES_PATH . '/_header.php');
+
+?>
+
+<!-- Main content -->
+<div class="container container-contact">
+    <div class="row">
+        <div class="col-md-6 col-md-offset-3">
+            <h1 class="text-center">Get in touch with me</h1>
+            <p class="text-center">I will respond within 24 hours (Normally, within few hours)</p>
+            <hr>
+
+            <form class="contact-form" action="" method="post">
+                <label for="name">Your name</label>
+                <input required class="form-control" type="text" name="name" placeholder="Your name here">
+                <label for="email">Your email</label>
+                <input required class="form-control" type="email" name="email" placeholder="johndoe@mail.com">
+                <label for="subject">Subject</label>
+                <input required class="form-control" type="text" name="subject" placeholder="Message subject">
+                <label for="body">Message</label>
+                <textarea required class="form-control" name="body" rows="8" cols="80"></textarea>
+                <button class="btn btn-danger btn-block" type="submit" name="submit">Send</button>
+            </form>
+
+        </div>
+    </div>
+</div>
+
+<?php include(TEMPLATES_PATH . '/_footer.php') ?>
