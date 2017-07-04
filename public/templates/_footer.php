94cebc6b11fbc5d29417940c4ac79fb681790650
Blaz Petek <petekblaz94@gmail.com>
2017-07-04 12:34:44 +0200

    Included js file

--- public/templates/_footer.php
+++ public/templates/_footer.php
@@ -2,7 +2,10 @@
     <p class="text-center">Copyright © 2017 Blaz Petek</p>
 </footer>
 
-<!-- Bootstrap js -->
+<!-- Scripts -->
+<!--Bootstrap-->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<!--Admin article index page-->
+<script src="../js/admin-article-index.js"></script>
 </body>
 </html>
