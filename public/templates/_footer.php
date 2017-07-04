7cf07a2c5c36e336b378325453e33d8057dbb340
Blaz Petek <petekblaz94@gmail.com>
2017-07-04 20:28:24 +0200

    Removed closing body and html tags, so I can include my own js scripts on each seperate view page, instead of all together in here

--- public/templates/_footer.php
+++ public/templates/_footer.php
@@ -5,7 +5,4 @@
 <!-- Scripts -->
 <!--Bootstrap-->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-<!--Admin article index page-->
-<script src="../js/admin-article-index.js"></script>
-</body>
-</html>
+
