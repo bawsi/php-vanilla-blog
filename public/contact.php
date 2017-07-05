79160e1069cdfbd07e388b2d6265a97fa67aac92
Blaz Petek <petekblaz94@gmail.com>
2017-07-05 14:52:31 +0200

    Removed required attributes from form and added contact-errors div

--- public/contact.php
+++ public/contact.php
@@ -16,16 +16,16 @@ include(TEMPLATES_PATH . '/_header.php');
             <h1 class="text-center">Get in touch with me</h1>
             <p class="text-center">I do my best to respond within 24 hours</p>
             <hr>
-
+            <div id="contact-errors"></div>
             <form class="contact-form" action="" method="post">
                 <label for="name">Your name</label>
-                <input required class="form-control" type="text" name="name" placeholder="Your name here">
+                <input class="form-control" type="text" name="name" placeholder="Your name here">
                 <label for="email">Your email</label>
-                <input required class="form-control" type="email" name="email" placeholder="johndoe@mail.com">
+                <input class="form-control" type="email" name="email" placeholder="johndoe@mail.com">
                 <label for="subject">Subject</label>
-                <input required class="form-control" type="text" name="subject" placeholder="Message subject">
+                <input class="form-control" type="text" name="subject" placeholder="Message subject">
                 <label for="body">Message</label>
-                <textarea required class="form-control" name="body" rows="8" cols="80"></textarea>
+                <textarea class="form-control" name="body" rows="8" cols="80"></textarea>
                 <div class="g-recaptcha" data-sitekey="6LcugCYUAAAAAHgv0uxMTivL9kTM7PmBqz8reHkx"></div>
                 <button class="btn btn-danger btn-block" type="submit" name="submit">Send</button>
             </form>
@@ -35,5 +35,8 @@ include(TEMPLATES_PATH . '/_header.php');
 </div>
 
 <?php include(TEMPLATES_PATH . '/_footer.php') ?>
+<!--validate contact form-->
+<script src="js/validate-contact.js"></script>
+
 </body>
 </html>
