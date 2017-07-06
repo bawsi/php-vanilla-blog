1a3f1d61571702f0f339e8e151dffe0046a51647
Blaz Petek <petekblaz94@gmail.com>
2017-07-06 09:01:55 +0200

    Added js script that validates passwords, removed required attr from form elements, added errors div

--- public/admin/settings.php
+++ public/admin/settings.php
@@ -13,16 +13,17 @@ include(TEMPLATES_PATH . '/_header.php');
 ?>
 
 <!-- Main content -->
-<div class="container container-settings">
+<div class="container container-settings">va
 	<div class="row">
 		<!-- Settings form -->
 		<div class="col-md-4 col-md-offset-4">
             <h2>Update your password</h2>
             <hr>
+            <div id="settings-errors"></div>
 			<form class="settings-form" action="" method="post">
-                <input required class="form-control" type="password" name="old-password" placeholder="Enter your old password" style="margin-bottom:5px;">
-                <input required class="form-control" type="password" name="new-password" placeholder="New password" style="margin-bottom:5px;">
-                <input required class="form-control" type="password" name="new-password-verify" placeholder="Verify new password" style="margin-bottom:10px;">
+                <input class="form-control" type="password" name="old-password" placeholder="Enter your old password" style="margin-bottom:5px;">
+                <input class="form-control" type="password" name="new-password" placeholder="New password" style="margin-bottom:5px;">
+                <input class="form-control" type="password" name="new-password-verify" placeholder="Verify new password" style="margin-bottom:10px;">
                 <button type="submit" name="submit" class="btn btn-danger btn-block">Submit</button>
 			</form>
 		</div>
@@ -32,5 +33,7 @@ include(TEMPLATES_PATH . '/_header.php');
 <!-- End of main content -->
 
 <?php include(TEMPLATES_PATH . '/_footer.php'); ?>
+<!--Validate form-->
+<script src="../js/validate-settings.js"></script>
 </body>
 </html>
